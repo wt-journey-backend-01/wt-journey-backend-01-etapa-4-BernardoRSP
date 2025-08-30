a8ea229f4d5c64025d70e3f969ec064c23a9db94
Bernardo Rezende <bernardo.com5@gmail.com>
2025-08-30 13:38:03 -0300

    return next() adicionado

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,11 +2,14 @@ const jwt = require("jsonwebtoken");
 
 function authMiddleware(req, res, next) {
   try {
+    const cookieToken = req.cookies?.access_token;
     const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+    const headerToken = authHeader && authHeader.split(" ")[1];
+
+    const token = cookieToken || headerToken;
 
     if (!token) {
-      return res.status(401).json({ status: 401, message: "Token Necessário" });
+      return next(res.status(401).json({ status: 401, message: "Token Necessário" }));
     }
 
     req.user = jwt.verify(token, process.env.JWT_SECRET);
